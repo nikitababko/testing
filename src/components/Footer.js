188ae5581f78a56fae439eb9bc004545fac4e5bc
Nikita Babko <niki-babko@yandex.ru>
2020-12-08 19:13:21 +0700

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,28 +18,37 @@ class Footer extends Component {
                             <h3>Share with love</h3>
                             <div className="footer__share-social" target="_blank">
                                 <a href="https://www.facebook.com/">
-                                    <FontAwesomeIcon className="font-awesome-icon" icon={faFacebookF} />
+                                    <FontAwesomeIcon
+                                        className="font-awesome-icon"
+                                        icon={faFacebookF}
+                                    />
                                 </a>
                                 <a href="https://twitter.com/?lang=ru" target="_blank">
-                                    <FontAwesomeIcon className="font-awesome-icon" icon={faTwitter} />
+                                    <FontAwesomeIcon
+                                        className="font-awesome-icon"
+                                        icon={faTwitter}
+                                    />
                                 </a>
                                 <a href="https://ru.linkedin.com/" target="_blank">
-                                    <FontAwesomeIcon className="font-awesome-icon" icon={faLinkedinIn} />
+                                    <FontAwesomeIcon
+                                        className="font-awesome-icon"
+                                        icon={faLinkedinIn}
+                                    />
                                 </a>
                             </div>
                         </div>
                         <div className="footer__about">
                             <h3>About ActiveBox</h3>
                             <p>
-                                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec ullamcorper
-                                nulla non metus auctor fringilla.
+                                Cras justo odio, dapibus ac facilisis in, egestas eget
+                                quam. Donec ullamcorper nulla non metus auctor fringilla.
                             </p>
                         </div>
                     </div>
                     <div className="footer__rights">
                         <p>
-                            Copyright © 2015 ActiveBox. All Rights Reserved Made with <i class="fas fa-heart"></i>{" "}
-                            <span>by Kamal Chaneman</span>
+                            Copyright © 2015 ActiveBox. All Rights Reserved Made with{" "}
+                            <i class="fas fa-heart"></i> <span>by Kamal Chaneman</span>
                         </p>
                     </div>
                 </div>
