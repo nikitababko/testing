a1aaab2fdb41e34449b23734335f7b5cbbd3b745
Nikita Babko <niki-babko@yandex.ru>
2021-05-15 14:42:12 +0700

    Rebuild project.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import Request from './components/Request';
 
@@ -8,5 +9,30 @@ const App = () => {
     </div>
   );
 };
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+>>>>>>> df64d85... Initialize project using Create React App
 
 export default App;
