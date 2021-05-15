8fd6f773e42269fe768826e9ea29e0a5d2f11da2
Nikita Babko <niki-babko@yandex.ru>
2021-05-15 17:03:30 +0700

    Added main file for imports.

---
+++ src/hooks/index.js
@@ -0,0 +1,5 @@
+export { default as useDebounce } from './useDebounce';
+export { default as useHover } from './useHover';
+export { default as useInput } from './useInput';
+export { default as useRequest } from './useRequest';
+export { default as useScroll } from './useScroll';
